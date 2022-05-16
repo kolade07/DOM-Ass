3573b6b9f228a127e959d4b3d2ad5f9fea656af7
Abisoye Omokolade <68041455+kolade07@users.noreply.github.com>
2022-05-16 16:16:09 +0100

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,70 @@
+// 1
+
+let numArray = [];
+
+function numArray() {
+  for (var i = 1; i <= 100; i++) {
+        if (i%2 === 0) {
+            numArray.push(i);
+        }
+    }
+}
+  
+// 2
+
+var numArray = [];
+
+function numGame(A, B) {
+
+    if (A < B) {
+        for (i=A+1; i<B; i++) {
+            if (i%2 === 0) {
+                numArray.push(i);
+            }
+        }  
+    }  
+
+    else {
+        for(i=B+1; i<A; i++) {
+            if (i%2 === 0) {
+              numArray.push(i);
+            }          
+        }
+    } 
+}
+
+// 3
+
+D
+
+// 4
+function firstFunction(A,B) {
+    return A + B;
+}
+
+// 5
+function printSum(A,B) {
+   return A + B;
+}
+
+// 6
+
+function nameArr() {
+   return ["Olorunsola", "Okikijesu", "Precious", "Adaobi", "Iyin", "Oremu", "Sam", "Segun", "Tobi", "Victoria"]
+}
+
+// 7a
+let Arr = [1,2,4];
+  Arr.push(5);
+
+// 7b
+     let numArr = [1,2,3,8];
+     numArr.pop();
+
+// 7c
+     var Arr = [23.77,80,45];
+     Arr.shift();
+
+// 7d   
+     var newArr = [[20,30,80],[10,50,55]]; 
+     newArr.unshift(2);
