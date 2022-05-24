b80a058720aabc9311212f45d2ba3098c61aa504
Abisoye Omokolade <68041455+kolade07@users.noreply.github.com>
2022-05-24 13:05:04 +0100

    Update dom.js

--- dom.js
+++ dom.js
@@ -32,3 +32,39 @@ function getMusician(muse) {
             console.log("Not correct");
         }
 }
+
+
+// If you want to pass an Array into the function, use this:
+
+function getMusician(musicians) {
+
+    let muse = musicians.length;
+
+        if(muse <= 0) {
+            console.log('not a group');
+        }
+
+        else if(muse === 1) {
+            console.log('solo');
+        }
+
+        else if(muse === 2) {
+            console.log('duet');
+        }
+
+        else if(muse === 3) {
+            console.log("trio");
+        } 
+
+        else if(muse === 4) {
+            console.log("quartet");
+        }
+
+        else if(muse >= 4) {
+            console.log("large group");
+        }
+
+        else {
+            console.log("Not correct");
+        }
+}
